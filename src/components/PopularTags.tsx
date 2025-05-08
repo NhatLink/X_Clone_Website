766fd4b7d843d4e0e0ea91d5e058d1bfc4c98088
NhatLink <116991698+NhatLink@users.noreply.github.com>
2025-05-08 20:11:46 +0700

    create project and config 2 side bar

---
+++ src/components/PopularTags.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const PopularTags = () => {
+  return (
+    <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+      <h1 className="text-xl font-bold text-textGrayLight">
+        {"What's"} Happening
+      </h1>
+
+      {/* TREND EVENT */}
+      <div className="flex gap-4">
+        <div className="relative w-20 h-20 rounded-xl overflow-hidden">
+          <Image src="/general/post.jpg" alt="event" fill />
+        </div>
+        <div className="flex-1">
+          <h2 className="font-bold text-textGrayLight">
+            New event on Liyue !!! Check it now
+          </h2>
+          <span className="text-sm text-textGray">9 hour ago</span>
+        </div>
+      </div>
+
+      {/* TOPICS */}
+      {Array(4)
+        .fill(0)
+        .map((_, i) => (
+          <div key={i}>
+            <div className="flex items-center justify-between">
+              <span className="text-textGray text-sm">
+                Technology • Trending
+              </span>
+              <Image
+                src="/icons/infoMore.svg"
+                alt="info"
+                width={16}
+                height={16}
+              />
+            </div>
+            <h2 className="text-textGrayLight font-bold">OpenAI</h2>
+            <span className="text-textGray text-sm">20K posts</span>
+          </div>
+        ))}
+
+      <Link href="/" className="text-iconBlue">
+        Show More
+      </Link>
+    </div>
+  );
+};
+
+export default PopularTags;
---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors, // kế thừa toàn bộ màu mặc định của Tailwind
+        textGray: "#71767b",
+        textGrayLight: "#e7e9ea",
+        textWhite: "#fff",
+        textBlack: "#000",
+        borderGray: "#2f3336",
+        inputGray: "#202327",
+        iconBlue: "#1d9bf0",
+        iconGreen: "#00ba7c",
+        iconPink: "#f91880",
+      },
+    },
+    screens: {
+      xsm: "500px",
+      sm: "600px",
+      md: "690px",
+      lg: "988px",
+      xl: "1078px",
+      xxl: "1265px",
+    },
+  },
+  plugins: [],
+} satisfies Config;
